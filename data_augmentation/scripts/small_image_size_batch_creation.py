f77e2f852e553222fdb29f0b43cd66af7f2b4979
John Mays <25927827+johnmays@users.noreply.github.com>
2021-12-07 09:32:17 -0500

    a better script for testing FC and conv nets, plus a finished data augmentation script

--- data_augmentation/scripts/small_image_size_batch_creation.py
+++ data_augmentation/scripts/small_image_size_batch_creation.py
@@ -1,10 +1,12 @@
 from PIL import Image
 import os
 import matplotlib.pyplot as plt
-new_image_dimensions = (256, 256)
+crop_dimensions = (1024, 1024)  # 1024x1024 are the recommended largest dimensions
+pool = False  # set to true if you want pooling to take place
+pool_dimensions = (32, 32)  # 32x32 are the recommended smallest dimensions
 number_of_images_from_source = 1
 dispersion_times = ['01', '03', '08', '15']
-print("Dimension of new images: " + str(new_image_dimensions[0]) + " x " + str(new_image_dimensions[1]))
+print("Dimension of new images: " + str(crop_dimensions[0]) + " x " + str(crop_dimensions[1]))
 
 source_data_location = '/Users/johnmays/Documents/Wirth Lab/still_data/dataset_1/'
 augmented_data_location = '/Users/johnmays/Documents/Wirth Lab/still_data/small_datasets/'
@@ -13,7 +15,9 @@ os.chdir(source_data_location)
 # Create Directories:
 
 os.chdir(augmented_data_location)
-new_dir_name = str(new_image_dimensions[0]) + "_" + str(new_image_dimensions[1])
+new_dir_name = str(crop_dimensions[0]) + "_" + str(crop_dimensions[1])
+if pool:
+    new_dir_name = str(pool_dimensions[0]) + "_" + str(pool_dimensions[1]) + "_pooled_from_" + str(crop_dimensions[1])
 os.makedirs(new_dir_name)
 os.chdir(augmented_data_location + new_dir_name)
 for dispersion_time in dispersion_times:
@@ -32,5 +36,35 @@ for subdirectory in os.listdir(source_data_location):
                 number_of_images_in_subdirectory += 1
         if number_of_images_in_subdirectory < min_number_of_images_in_subdirectory:
             min_number_of_images_in_subdirectory = number_of_images_in_subdirectory
+
+# generating crop coordinates:
+crop_coordinates = (int(640-crop_dimensions[0]/2), int(512-crop_dimensions[1]/2), int(640+crop_dimensions[0]/2), int(512+crop_dimensions[1]/2))
+print(crop_coordinates)
+
+for dispersion_time in dispersion_times:
+    destination_dir = augmented_data_location + new_dir_name + "/" + dispersion_time
+    i = 0
+    for filename in os.listdir(source_data_location + dispersion_time):
+        i += 1
+        if i > min_number_of_images_in_subdirectory:
+            break
+        elif filename.endswith('.tif'):
+            os.chdir(source_data_location + dispersion_time)
+            img = Image.open(filename)
+            os.chdir(destination_dir)
+            cropped_img = img.crop(crop_coordinates)  # L, T, R, B (does not include pixel on bottom-right coordinate)
+            if not pool:
+                cropped_img.save(filename[0:-4] + '_' + str(crop_dimensions[0]) + '_cropped_0' + '.tif')
+                cropped_img.rotate(90).save(filename[0:-4] + '_' + str(crop_dimensions[0]) + '_cropped_90' + '.tif')
+                cropped_img.rotate(180).save(filename[0:-4] + '_' + str(crop_dimensions[0]) + '_cropped_180' + '.tif')
+                cropped_img.rotate(270).save(filename[0:-4] + '_' + str(crop_dimensions[0]) + '_cropped_270' + '.tif')
+            else:
+                resized_img = cropped_img.resize(pool_dimensions)
+                resized_img.save(filename[0:-4] + '_' + str(pool_dimensions[0]) + '_cropped_pooled_0' + '.tif')
+                resized_img.rotate(90).save(filename[0:-4] + '_' + str(pool_dimensions[0]) + '_cropped_pooled_90' + '.tif')
+                resized_img.rotate(180).save(filename[0:-4] + '_' + str(pool_dimensions[0]) + '_cropped_pooled_180' + '.tif')
+                resized_img.rotate(270).save(filename[0:-4] + '_' + str(pool_dimensions[0]) + '_cropped_pooled_270' + '.tif')
+        else:
+            continue
 
 
---
+++ still_paint/small_local_testing/new scripts/CNN_local_testing.py
@@ -0,0 +1,44 @@
+# Some parts of this code are taken from SentDex's tutorial on importing image files for use in tf: https://pythonprogramming.net/loading-custom-data-deep-learning-python-tensorflow-keras/
+
+import tensorflow as tf
+from tensorflow import keras
+import os
+import matplotlib.pyplot as plt
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+# For the HPC:
+# data_directory = '/home/jkm100/augmented_dataset_1'
+data_directory = '/Users/johnmays/Documents/Wirth Lab/still_data/small_datasets/32_32_pooled_from_1024'
+dispersion_times = ['01', '03', '08', '15']  # these are the categories
+os.chdir(data_directory)
+os.getcwd()
+
+train_batches = keras.preprocessing.image.ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=data_directory, target_size=(32, 32), classes=dispersion_times, batch_size=64)
+
+
+# fully connected network for 2D images:
+fc_network = keras.Sequential([
+    keras.layers.Flatten(),
+    keras.layers.Dense(units=64, activation=tf.nn.relu),
+    keras.layers.Dense(units=64, activation=tf.nn.relu),
+    keras.layers.Dense(units=4, activation=tf.nn.softmax)
+])
+
+
+# conv network for 2Dx3 images:
+conv_network = keras.Sequential([
+    tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)),
+    tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2),
+    tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same'),
+    tf.keras.layers.MaxPool2D(pool_size=(2, 2), strides=2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(units=4, activation='softmax')
+])
+
+# conv_network.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])  # metrics=['accuracy']
+
+# conv_network.fit(x=train_batches, epochs=20, verbose=2)
+
+fc_network.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])  # metrics=['accuracy']
+
+fc_network.fit(x=train_batches, epochs=20, verbose=2)
