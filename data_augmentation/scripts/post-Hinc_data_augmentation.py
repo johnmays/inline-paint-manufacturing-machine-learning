2f85279cc8bafee06862813f87af1946b33eeee0
John Mays <25927827+johnmays@users.noreply.github.com>
2022-02-11 18:35:00 -0500

    CURRENTLY NONFUNCTIONAL - rewriting data augmentation script to use more of the image

--- data_augmentation/scripts/post-Hinc_data_augmentation.py
+++ data_augmentation/scripts/post-Hinc_data_augmentation.py
@@ -1,10 +1,20 @@
+# Necessary Imports:
+
 from PIL import Image
 import os
 import matplotlib.pyplot as plt
+import math
+
+"""Note: the purpose of this script is to parse the source data directories, full of .TIFs, and for each .TIF, \
+chop it up into equally sized subimages, then take each subimage and produce 4 separate rotated images, and finally put all  \
+of them into the approprate new directory. 
+"""
+
+source_image_dimensions = (1280, 1024)  # the dimensions of the source images (5:4)
 crop_dimensions = (256, 256)  # 1024x1024 are the recommended largest dimensions
 pool = False  # set to true if you want pooling to take place
 pool_dimensions = (256, 256)  # 32x32 are the recommended smallest dimensions
-number_of_images_from_source = 1
+number_of_images_from_source = 20
 dispersion_times = ['01', '03', '08', '15']
 print("Dimension of new images: " + str(crop_dimensions[0]) + " x " + str(crop_dimensions[1]))
 
@@ -15,16 +25,16 @@ os.chdir(source_data_location)
 # Create Directories:
 
 os.chdir(augmented_data_location)
-new_dir_name = str(crop_dimensions[0]) + "_" + str(crop_dimensions[1])
+new_dir_name = str("Post-Hinc" + crop_dimensions[0]) + "_" + str(crop_dimensions[1])
 if pool:
-    new_dir_name = str(pool_dimensions[0]) + "_" + str(pool_dimensions[1]) + "_pooled_from_" + str(crop_dimensions[1])
+    new_dir_name = new_dir_name + "pooled_to" + str(pool_dimensions[0]) + "_" + str(pool_dimensions[1])
 os.makedirs(new_dir_name)
 os.chdir(augmented_data_location + new_dir_name)
 for dispersion_time in dispersion_times:
   os.makedirs(dispersion_time)
 
 
-# Find smallest number of images in one source directory
+# Find smallest number of images in a source directory; which will be the limiting number, in order to evenly represent the dispersion times
 min_number_of_images_in_subdirectory = 1000000
 
 os.chdir(source_data_location)
@@ -38,9 +48,19 @@ for subdirectory in os.listdir(source_data_location):
             min_number_of_images_in_subdirectory = number_of_images_in_subdirectory
 
 # generating crop coordinates:
-crop_coordinates = (int(640-crop_dimensions[0]/2), int(512-crop_dimensions[1]/2), int(640+crop_dimensions[0]/2), int(512+crop_dimensions[1]/2))
-print(crop_coordinates)
+crop_coordinates = []
+top_corner = (0,0)
+bottom_corner = crop_dimensions
 
+for x in range(math.floor(source_image_dimensions[0]/crop_dimensions[0])):
+    for y in range(math.floor(source_image_dimensions[1]/crop_dimensions[1])):
+
+
+
+# crop_coordinates = (int(640-crop_dimensions[0]/2), int(512-crop_dimensions[1]/2), int(640+crop_dimensions[0]/2), int(512+crop_dimensions[1]/2))
+# print(crop_coordinates)
+
+"""
 for dispersion_time in dispersion_times:
     destination_dir = augmented_data_location + new_dir_name + "/" + dispersion_time
     i = 0
@@ -66,5 +86,5 @@ for dispersion_time in dispersion_times:
                 resized_img.rotate(270).save(filename[0:-4] + '_' + str(pool_dimensions[0]) + '_cropped_pooled_270' + '.tif')
         else:
             continue
+"""
 
-        
